8e0439a4dd81a717741a49b02dba901602a314ce
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-12-03 12:20:52 +0100

    initialisation of project.

---
+++ README.md
@@ -0,0 +1,14 @@
+# installation
+
+if it is not installed (but it should be in the vendor folder), install 
+[github.com/jinzhu/copier](github.com/jinzhu/copier) by executing these commands :
+
+```
+govendor init
+govendor fetch github.com/jinzhu/copier
+```
+
+and to run the sudoku : 
+```
+go run sudoku.go
+```
---
+++ sudoku.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jinzhu/copier"
+)
+
+//structure for the grid
+type Grid struct{
+	Values [9][9]int
+}
+
+//some methods to interact with our grid
+func (this *Grid) addSolvingNumber(solving int, x int, y int) (bool, Grid){
+	//first we'll copy the current grid to get a new one
+	copiedGrid := Grid{}
+
+	copier.Copy(&copiedGrid, &this)
+
+	//check the box is empty
+	if this.Values[y][x]  != 0{
+		return false, copiedGrid
+	}
+
+	//check if the number is in the row
+	if intInSlice(solving, this.Values[y]){
+		return false, copiedGrid
+	}
+
+	copiedGrid.Values[y][x] = solving
+	return true, copiedGrid
+}
+
+//helper function to check if int is in list
+func intInSlice(a int, list [9]int) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+func main()  {
+	fmt.Println("hey")
+
+	//filling the grid with numbers
+	var currentGrid = Grid{Values:[9][9]int{
+		{5, 3, 0, 0, 7, 0, 0, 0, 0},
+		{6, 0, 0, 1, 9, 5, 0, 0, 0},
+		{0, 9, 8, 0, 0, 0, 0, 6, 0},
+		{8, 0, 0, 0, 6, 0, 0, 0, 3},
+		{4, 0, 0, 8, 0, 3, 0, 0, 1},
+		{7, 0, 0, 0, 2, 0, 0, 0, 6},
+		{0, 6, 0, 0, 0, 0, 2, 8, 0},
+		{0, 0, 0, 4, 1, 9, 0, 0, 5},
+		{0, 0, 0, 0, 8, 0, 0, 7, 9},
+	}}
+
+	_, nextGrid := currentGrid.addSolvingNumber(6, 2, 0)
+
+	fmt.Println(currentGrid)
+	fmt.Println(nextGrid)
+}
