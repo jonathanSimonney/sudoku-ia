65007577c0cd1f76614da5f51ee92e14a4ea499a
Jonathan SIMONNEY <jonathan.simonney@gmail.com>
2018-12-07 06:43:18 +0100

    began work to use pointers and tree of possibility instead of array of possibility.

--- sudoku.go
+++ sudoku.go
@@ -24,12 +24,13 @@ type Possibility struct{
 	Numbers []int
 	X int
 	Y int
+	LinkedPossibilities []*Possibility
 }
 
 //structure for the grid
 type Grid struct{
 	Values [9][9]int
-	Possibilities []Possibility
+	Possibilities []*Possibility
 }
 
 //some methods to interact with our grid
@@ -88,6 +89,47 @@ func (this *Grid) getLegalNumbersAtPos(x int, y int)(legalValues []int){
 	return legalValues
 }
 
+//a helper to check if a value is valid at a position with performance in mind
+func (this *Grid) isValueValidThere(x int, y int, solving int) (isValid bool){
+	for i := 0; i < 9; i++{
+		inLineForbidden := this.Values[y][i]
+		if inLineForbidden == solving{
+			return false
+		}
+
+		inColumnForbidden := this.Values[i][x]
+		if inColumnForbidden == solving{
+			return false
+		}
+	}
+
+	//first, know which square is targetted
+	xSquareStart := (x / 3) * 3
+	ySquareStart := (y / 3) * 3
+
+	xSquareEnd := ((x / 3) * 3) + 3
+	ySquareEnd := ((y / 3) * 3) + 3
+
+	//then get all the numbers in this square
+	for yIndex := ySquareStart; yIndex < ySquareEnd; yIndex++{
+		for xIndex := xSquareStart; xIndex < xSquareEnd; xIndex++{
+			forbidden := this.Values[yIndex][xIndex]
+			if forbidden == solving{
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+//helper to suppress a value from the possibility linked to one whith the value assigned to the possibility
+func (this *Grid) suppressFromLinkedPossibility(possibility Possibility, assignedValue int){
+	for _, linkedPossibility := range possibility.LinkedPossibilities{
+		linkedPossibility.Numbers = suppressFirstFromSlice(assignedValue, linkedPossibility.Numbers)
+	}
+}
+
 //an adder of number which checks if the number can be added
 func (this *Grid) addSolvingNumber(solving int, x int, y int, trustSolving bool) (isValid bool, modifiedGrid Grid){
 	//check the box is empty
@@ -95,19 +137,12 @@ func (this *Grid) addSolvingNumber(solving int, x int, y int, trustSolving bool)
 		return false, *this
 	}
 
-	if trustSolving{
-		this.Values[y][x] = solving
-		return true, *this
-	}else{
-		invalidValues := this.getIncludingSets(x, y, false)
-
-		if intInSlice(solving, invalidValues){
-			return false, *this
-		}
-
-		this.Values[y][x] = solving
-		return true, *this
+	if !trustSolving && !this.isValueValidThere(x, y, solving){
+		return false, *this
 	}
+
+	this.Values[y][x] = solving
+	return true, *this
 }
 
 //a pretty print
@@ -162,6 +197,17 @@ func (this *Grid) emptyGrid(){
 	}
 }
 
+//a helper to add the subPossibility array to possibilities in grid
+func (this* Grid) addPossibilitiesSubPossibilities(){
+	var futurePossibilities []*Possibility
+	for _, possibility := range this.Possibilities{
+
+		futurePossibilities = append(futurePossibilities, possibility)
+	}
+
+	this.Possibilities = futurePossibilities
+}
+
 //a helper to fill the possibles list for our grid
 func (this *Grid) prepare() (isValid bool){
 	for y := range make([]int, 9){
@@ -171,7 +217,7 @@ func (this *Grid) prepare() (isValid bool){
 			    if len(legalNumbers) == 1{
 			    	this.Values[y][x] = legalNumbers[0]
 				}else{
-					this.Possibilities = append(this.Possibilities, Possibility{X: x, Y: y, Numbers: legalNumbers})
+					this.Possibilities = append(this.Possibilities, &Possibility{X: x, Y: y, Numbers: legalNumbers})
 				}
 			}else{
 				forbiddenValues := this.getIncludingSets(x, y, false)
@@ -184,22 +230,12 @@ func (this *Grid) prepare() (isValid bool){
 
 	//sort.Sort(byPossibility(this.Possibilities))
 
-	//fmt.Println(this.Possibilities)
+	this.addPossibilitiesSubPossibilities()
+
+	fmt.Println(this.Possibilities)
+	panic("stop")
 
 	return true
-}
-
-//a helper to get the coords AND the list of elems of the best cell
-func (this *Grid) getPossibility(index int) (x int, y int, legalValues []int, isSolved bool){
-	possibilityNumberLeft := len(this.Possibilities)
-
-	if possibilityNumberLeft == index{//there isn't any possibility left
-		return -1, -1, []int{}, true
-	}
-
-	nextPossibility := this.Possibilities[index]
-
-	return nextPossibility.X, nextPossibility.Y, nextPossibility.Numbers, false
 }
 
 //solver for the grid
@@ -213,18 +249,25 @@ func recursivelySolveGrid(grid Grid, randomly bool, firstTime bool, index int) (
 		fmt.Println("grid prepared in ")
 		fmt.Println(time.Since(prepareBegin))
 	}
-	x, y, arrayIter, isSolved := grid.getPossibility(index)
-
-	//it avoids to check if the number is valid in addSolvingNumber...
-	if x != -1{
-		arrayIter = grid.getLegalNumbersAtPos(x, y)
-	}
-
 
 	//if the sudoku is already filled, nothing to do
-	if isSolved{
+	if len(grid.Possibilities) == index{
 		return true, grid
 	}
+
+	nextPossibility := grid.Possibilities[index]
+
+	var arrayIter []int
+	x := nextPossibility.X
+	y := nextPossibility.Y
+	//arrayIter = nextPossibility.Numbers
+
+	//x, y, arrayIter, isSolved := grid.getPossibility(index)
+
+	//it avoids to check if the number is valid in addSolvingNumber...
+	//if x != -1{
+	//	arrayIter = grid.getLegalNumbersAtPos(x, y)
+	//}
 
 	//shuffle the array of numbers to test in case the user wants random
 	if randomly{
@@ -233,6 +276,7 @@ func recursivelySolveGrid(grid Grid, randomly bool, firstTime bool, index int) (
 
 	//test each value in the array of valid numbers.
 	for _, solvingValue := range arrayIter{
+		currentPossibilities := grid.Possibilities
 		addSuccessfull, newGrid := grid.addSolvingNumber(solvingValue, x, y, true)
 		if addSuccessfull{
 			isSolved, solvedGrid := recursivelySolveGrid(newGrid, randomly, false, index + 1)
@@ -241,6 +285,7 @@ func recursivelySolveGrid(grid Grid, randomly bool, firstTime bool, index int) (
 			}
 		}
 		grid.Values[y][x] = 0
+		grid.Possibilities = currentPossibilities
 	}
 
 	//fmt.Println("sorry...")
@@ -300,26 +345,27 @@ func programMain()  {
 	//filling the grid with numbers
 	var currentGrid = Grid{Values:[9][9]int{
 		//20 - 30 second OR 1 second with sort
-		{0, 0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 3, 0, 8, 5},
-		{0, 0, 1, 0, 2, 0, 0, 0, 0},
-		{0, 0, 0, 5, 0, 7, 0, 0, 0},
-		{0, 0, 4, 0, 0, 0, 1, 0, 0},
-		{0, 9, 0, 0, 0, 0, 0, 0, 0},
-		{5, 0, 0, 0, 0, 0, 0, 7, 3},
-		{0, 0, 2, 0, 1, 0, 0, 0, 0},
-		{0, 0, 0, 0, 4, 0, 0, 0, 9},
+		//{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		//{0, 0, 0, 0, 0, 3, 0, 8, 5},
+		//{0, 0, 1, 0, 2, 0, 0, 0, 0},
+		//{0, 0, 0, 5, 0, 7, 0, 0, 0},
+		//{0, 0, 4, 0, 0, 0, 1, 0, 0},
+		//{0, 9, 0, 0, 0, 0, 0, 0, 0},
+		//{5, 0, 0, 0, 0, 0, 0, 7, 3},
+		//{0, 0, 2, 0, 1, 0, 0, 0, 0},
+		//{0, 0, 0, 0, 4, 0, 0, 0, 9},
 
-		//insolvable originally long grid 10 - 15 seconds OR infinite with sort
-		//{7, 0, 0, 0, 0, 0, 0, 0, 0},
-		//{0, 0, 0, 0, 0, 0, 7, 0, 0},
-		//{0, 0, 0, 0, 1, 2, 0, 0, 0},
-		//{0, 0, 0, 7, 0, 0, 0, 0, 0},
-		//{0, 0, 0, 0, 0, 0, 0, 0, 0},
-		//{1, 0, 0, 0, 0, 0, 0, 0, 0},
-		//{0, 0, 8, 0, 0, 0, 0, 0, 0},
-		//{0, 0, 0, 0, 0, 0, 5, 0, 0},
-		//{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		//infinite (with or without sort)
+		{0, 0, 0, 7, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{1, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 8, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 5, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{7, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 7, 0, 0},
+		{0, 0, 0, 0, 1, 2, 0, 0, 0},
+
 
 		//1 ms
 		//{0, 0, 0, 0, 0, 0, 0, 0, 0},
@@ -363,7 +409,7 @@ func programMain()  {
 		//{0, 0, 0, 0, 0, 0, 0, 0, 5},
 		//{0, 0, 0, 0, 0, 0, 1, 0, 0},
 		//{0, 0, 0, 0, 0, 5, 0, 0, 0},
-		//{0, 5, 0, 0, 0, 0, 0, 0, 0},
+		//{6, 5, 7, 8, 9, 2, 0, 3, 4},
 
 		//hardest in the world : 3 mSec
 		//{1, 0, 0, 0, 0, 7, 0, 9, 0},
@@ -387,7 +433,7 @@ func programMain()  {
 	solvedGrid.prettyPrint()
 	//currentGrid.fillGrid(5)
 	//currentGrid.prettyPrint()
-	//_, solvedGrid = recursivelySolveGrid(currentGrid, false, true)
+	//_, solvedGrid = recursivelySolveGrid(currentGrid, false, true, 0)
 	//solvedGrid.prettyPrint()
 }
 
